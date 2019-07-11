621798c288ebcdfe36014ec7ca789fbc94ca300b
Jakub Kondraciuk <kondraciuk.dev@gmail.com>
2019-07-11 20:26:56 +0200

    Added dogs API and loading images via axios library
    Installed axios,
    Added loading next images
    Added heart icon for saving images

--- src/components/ImageDisplayer/ImageDisplayer.vue
+++ src/components/ImageDisplayer/ImageDisplayer.vue
@@ -2,22 +2,58 @@
     <div class="imgd-wrapper">
         <ImageType class="imgd-wrapper__type" />
         <div class="imgd">
-            <div class="imgd__img"></div>
+            <div class="imgd__img"
+                 :style="style"
+                 role="img"
+                 aria-label="An image of choosen type of animal"
+                 title="An image of choosen type of animal" />
             <div class="imgd__bar">
-                <span class="imgd__icon">i</span>
-                <button class="btn btn-secondary">Next one</button>
+                <img :class="['imgd__icon', { 'imgd__icon--clicked' : saved }]"
+                     src="@/assets/icons/heart.png"
+                     alt="Heart icon"
+                     @click="saveImage">
+                <button class="btn btn-secondary imgd__button"
+                        @click="loadImage">Next one</button>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
 import ImageType from '@/components/ImageType/ImageType';
+
+const dogsAPI = 'https://dog.ceo/api/breeds/image/random';
 
 export default {
     name: 'ImageDisplayer',
     components: {
         ImageType
+    },
+    data () {
+        return {
+            saved: false,
+            imageURL: null
+        }
+    },
+    methods: {
+        loadImage () {
+            axios.get(dogsAPI)
+                .then(response => this.imageURL = response.data.message)
+                .catch(error => console.log(error))
+            this.saved = false;
+        },
+        saveImage () {
+            this.saved = true;
+        }
+    },
+    computed: {
+        style () {
+            return `background-image: url(${this.imageURL})`;
+        }
+    },
+    mounted () {
+        this.loadImage();
     }
 }
 </script>
