4dc01f1bdb8522b242a5ea05d8178df2037bbf24
Jakub Kondraciuk <kondraciuk.dev@gmail.com>
2019-07-15 19:27:41 +0200

    Styling a no-images tile in image gallery

--- src/components/SavedImages/SavedImages.vue
+++ src/components/SavedImages/SavedImages.vue
@@ -4,6 +4,7 @@
             <div class="row no-gutters">
                 <div class="col-12 col-sm-6 col-md-4 savedimages-wrapper__noimages">
                     <p class="savedimages-wrapper__paragraph">You haven't saved any photos yet</p>
+                    <img src="@/assets/icons/add.svg" alt="Add icon">
                 </div>
                 <div class="col-12 col-sm-6 col-md-4 savedimages-wrapper__image"
                      v-for="(image, index) in getSavedImages" :key="index">
