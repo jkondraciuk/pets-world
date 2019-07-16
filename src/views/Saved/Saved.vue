8812324888f6b0f2173e64ce42e760ecb600a606
Jakub Kondraciuk <kondraciuk.dev@gmail.com>
2019-07-16 19:35:01 +0200

    Added PageFooter to Saved view

--- src/views/Saved/Saved.vue
+++ src/views/Saved/Saved.vue
@@ -1,18 +1,21 @@
 <template>
     <div class="saved-wrapper">
         <SavedImages />
+        <PageFooter />
     </div>
 </template>
 
 <script>
-import SavedImages from '@/components/SavedImages/SavedImages';
+import SavedImages from '@/components/SavedImages/SavedImages'
+import PageFooter from '@/components/PageFooter/PageFooter'
 
 export default {
     name: 'Saved',
     components: {
         SavedImages,
+        PageFooter
     }
 }
 </script>
 
-<style src="./Saved.scss" lang="scss" scoped />>
+<style src="./Saved.scss" lang="scss" scoped />
